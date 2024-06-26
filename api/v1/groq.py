9d228f9d9987353adc7f1b5159d125c833016fc8
Gong Jianwei <1619794103@qq.com>
2024-06-26 09:57:58 +0800

    Update groq.py

--- api/v1/groq.py
+++ api/v1/groq.py
@@ -1,29 +1,20 @@
 #!/usr/bin/env python
-from pydantic import BaseModel, Field
-import httpx
-from fastapi.responses import JSONResponse
-from fastapi import FastAPI, File, Depends, Header, UploadFile, Form, HTTPException
-from fastapi.routing import APIRouter
-from openai import AsyncClient
 import typing
+import pydantic
+from fastapi import Header, APIRouter
+from openai import AsyncClient
 
 router = APIRouter()
 
-
-class GroqArgs(BaseModel):
-    api_key: str
+class ChatArgs(pydantic.BaseModel):
     model: str
     messages: typing.List[typing.Dict[str, str]]
 
-
-@router.post("/text")
-async def test_groq(args: GroqArgs,authorization: str = Header(...)):
-    client = AsyncClient(
-        base_url="https://api.groq.com/openai/v1",
-        api_key=args.api_key
-    )
+@router.post("/lovegroq")
+async def groq_api(args: ChatArgs, authorization: str = Header(...)):
+    api_key = authorization.split(" ")[1]
+    client = AsyncClient(base_url="https://api.groq.com/openai/v1", api_key=api_key)
     return await client.chat.completions.create(
         model=args.model,
         messages=args.messages,
     )
-
